3f7492512a75f2d5fc58a87bb72ffff874a1c159
Joshua Neff <jneffit@gmail.com>
2020-11-20 15:03:40 -0500

    Updated docstrings

--- TerritoryWars/TerritoryWars/Factions.cs
+++ TerritoryWars/TerritoryWars/Factions.cs
@@ -2,6 +2,9 @@
 {
     using System.ComponentModel;
 
+    /// <summary>
+    /// enum <c>Factions</c> contains all the usable factions for the plugin.
+    /// </summary>
     public enum Factions
     {
         [Description("Police")]
@@ -38,27 +41,49 @@
         VarriosLosAztecas = 15,
     }
 
+    /// <summary>
+    /// stuct <c>FactionData</c> is a container used for storing faction data.
+    /// </summary>
     public struct FactionData
     {
-        // Constructor
+        /// <summary>
+        /// method <c>Factiondata</c> constructor.
+        /// </summary>
+        /// <param name="NewFaction"></param>
         public FactionData(Factions NewFaction)
         {
             Faction = NewFaction;
         }
 
+        // Properties
+
+        /// <summary>
+        /// property <c>Faction</c> stores the faction type
+        /// </summary>
         public Factions Faction { get; }
 
+        // Methods
+
+        /// <summary>
+        /// method <c>ToString</c> returns a stringified version of the struct data.
+        /// </summary>
+        /// <returns>a stringified version of the struct data.</returns>
         public override string ToString() => $"({Faction})";
     }
 
+    /// <summary>
+    /// class <c>FactionsManager</c> is a singleton class used for managing the active factions for the plugin.
+    /// </summary>
     sealed class FactionsManager
     {
-        // Constructor
+        /// <summary>
+        /// method <c>FactionsManager</c> constructor.
+        /// </summary>
         public FactionsManager() : base()
         {
         }
 
-        // Singleton
+        // --> Singleton
         public static FactionsManager Instance
         {
             get
@@ -76,16 +101,29 @@
 
         private static FactionsManager instance = null;
         private static readonly object padlock = new object();
+        // <-- Singleton
     }
 
+    /// <summary>
+    /// class <c>FactionFactory</c> is used for creating factions.
+    /// </summary>
     class FactionFactory : Factory
     {
-        // Constructor
+        /// <summary>
+        /// method <c>FactionFactory</c> constructor.
+        /// </summary>
         public FactionFactory() : base()
         {
 
         }
 
+        // Methods
+
+        /// <summary>
+        /// method <c>CreateFaction</c> creates a new faction object.
+        /// </summary>
+        /// <param name="NewFactionData"></param>
+        /// <returns>A new faction object</returns>
         public Faction CreateFaction(FactionData NewFactionData)
         {
             Faction NewFaction = new Faction(NewFactionData);
@@ -93,16 +131,33 @@
         }
     }
 
+    /// <summary>
+    /// class <c>Faction</c> is the base class for a faction.
+    /// </summary>
     class Faction : TWClass
     {
-        // Constructor
+        /// <summary>
+        /// method <c>Faction</c> constructor.
+        /// </summary>
+        /// <param name="FactionData"></param>
         public Faction(FactionData FactionData) : base()
         {
             Data = FactionData;
         }
 
+        // Properties
+
+        /// <summary>
+        /// property <c>Data</c> stores the faction data for the faction object.
+        /// </summary>
         private static FactionData Data;
 
+        // Methods
+
+        /// <summary>
+        /// method <c>GetData</c> returns the faction data for the faction object.
+        /// </summary>
+        /// <returns>Faction data for the faction object.</returns>
         public FactionData GetData()
         {
             return Data;
