c133345df0926d7758cc81d9cee00f7bf7816de2
Joshua Neff <jneffit@gmail.com>
2020-11-20 22:46:15 -0500

    Added an AlliedFactions to FactionsData so the game state can understand how to process territory interaction

--- TerritoryWars/TerritoryWars/Factions.cs
+++ TerritoryWars/TerritoryWars/Factions.cs
@@ -11,8 +11,8 @@
     /// </summary>
     public enum Factions
     {
-        [Description("Los Santos Police")]
-        LosSantosPolice = 0,
+        [Description("Police")]
+        Police = 0,
         [Description("FIB")]
         FIB = 1,
         [Description("Military")]
@@ -54,6 +54,19 @@
     }
 
     /// <summary>
+    /// enum <c>FactionTypes</c> are used for things like allies and enemies.
+    /// </summary>
+    public enum FactionTypes
+    {
+        LawEnforcement,
+        Gang,
+        Mob,
+        Cartel,
+        Hillbilly,
+        MotorCycleClub,
+    }
+
+    /// <summary>
     /// stuct <c>FactionData</c> is a container used for storing faction data.
     /// </summary>
     public struct FactionData
@@ -67,72 +80,89 @@
             Faction = NewFaction;
             Territories = new List<Territories.TerritoryData>();
             Influence = 1.0f;
+            AlliedFactions = new List<FactionTypes>();
 
             // I use this switch statement as a means to set up factions. It's ugly and I don't care right now.
             switch (Faction)
             {
                 case Factions.ArmenianMob:
+                    FactionType = FactionTypes.Mob;
                     TerritoryColor = Color.Yellow;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.Ballas:
+                    FactionType = FactionTypes.Gang;
                     TerritoryColor = Color.Purple;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     Territories.Add(Headquarters);
                     break;
                 case Factions.FIB:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.Brown;
                     Influence = 3.0f;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(-429.2342f, 1109.349f, 327.682f), TerritoryColor, Influence);
                     Territories.Add(Headquarters);
                     break;
                 case Factions.GambettiCrimeFamily:
+                    FactionType = FactionTypes.Mob;
                     TerritoryColor = Color.Maroon;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.Hippies:
+                    FactionType = FactionTypes.Hillbilly;
                     TerritoryColor = Color.Green;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.Kkangpae:
+                    FactionType = FactionTypes.Gang;
                     TerritoryColor = Color.Red;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.LosSantosTriads:
+                    FactionType = FactionTypes.Gang;
                     TerritoryColor = Color.DarkRed;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.LosSantosVagos:
+                    FactionType = FactionTypes.Gang;
                     TerritoryColor = Color.Orange;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.MadrazoCartel:
+                    FactionType = FactionTypes.Cartel;
                     TerritoryColor = Color.PaleGreen;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.MarabuntaGrande:
+                    FactionType = FactionTypes.Gang;
                     TerritoryColor = Color.Blue;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.Military:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.Olive;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.ONeilBrothers:
+                    FactionType = FactionTypes.Mob;
                     TerritoryColor = Color.CadetBlue;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
-                case Factions.LosSantosPolice:
+                case Factions.Police:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.DodgerBlue;
                     Influence = 10.0f;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(451.7062f, -993.2839f, 30.6896f), TerritoryColor, Influence);
@@ -144,27 +174,34 @@
                     Territories.Add(new Territories.TerritoryData(Faction, new Vector3(-561.687f, -130.9729f, 38.43186f), TerritoryColor, Influence));
                     break;
                 case Factions.Rednecks:
+                    FactionType = FactionTypes.Hillbilly;
                     TerritoryColor = Color.IndianRed;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.TheLostMC:
+                    FactionType = FactionTypes.MotorCycleClub;
                     TerritoryColor = Color.Black;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.VarriosLosAztecas:
+                    FactionType = FactionTypes.Gang;
                     TerritoryColor = Color.SeaGreen;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     //Territories.Add(Headquarters);
                     break;
                 case Factions.NationalSecurityEnforcement:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.SkyBlue;
                     Influence = 10.0f;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(-863.4155f, -2409.932f, 14.02572f), TerritoryColor, Influence);
                     Territories.Add(Headquarters);
                     break;
                 case Factions.Sheriff:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.LimeGreen;
                     Influence = 10.0f;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(360.7052f, -1583.961f, 29.29195f), TerritoryColor, Influence);
@@ -174,18 +211,23 @@
                     Territories.Add(new Territories.TerritoryData(Faction, new Vector3(1848.665f, 3690.305f, 34.26707f), TerritoryColor, Influence));
                     break;
                 case Factions.ForestRangers:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.ForestGreen;
                     Influence = 10.0f;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(378.9325f, 792.2542f, 190.407f), TerritoryColor, Influence);
                     Territories.Add(Headquarters);
                     break;
                 case Factions.PenitentiaryGuards:
+                    FactionType = FactionTypes.LawEnforcement;
+                    AlliedFactions.Add(FactionTypes.LawEnforcement);
                     TerritoryColor = Color.RosyBrown;
                     Influence = 10.0f;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(1845.719f, 2586.033f, 45.67205f), TerritoryColor, Influence);
                     Territories.Add(Headquarters);
                     break; 
                 default:
+                    FactionType = FactionTypes.Hillbilly;
                     TerritoryColor = Color.White;
                     Headquarters = new Territories.TerritoryData(Faction, new Vector3(), TerritoryColor);
                     break;
@@ -218,6 +260,17 @@
         /// property <c>Influence</c> is used to determine the strength of a faction. Used in things like creating territories.
         /// </summary>
         public float Influence { get; }
+
+        /// <summary>
+        /// property <c>FactionType</c> is used to distinquish a type for things like allies and enemies.
+        /// </summary>
+        public FactionTypes FactionType { get; }
+
+        /// <summary>
+        /// property <c>AlliedFactions</c> is a list that contains the factions this faction is allined with.
+        ///     Anyone not allied is an enemy.
+        /// </summary>
+        public List<FactionTypes> AlliedFactions { get; }
 
         // Methods
 
